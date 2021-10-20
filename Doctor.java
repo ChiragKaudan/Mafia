5686adfc42a591fb6d0e7e5a7caf7c4efb295c42
Tanya Gupta <tanya.gupta@gmail.com>
2021-10-19 18:43:26 -0700

    Doctor and Player classes created and added.

---
+++ Doctor.java
@@ -0,0 +1,15 @@
+package mafia;
+
+public class Doctor extends Role
+{
+    public Doctor()
+    {
+        this.isTown=true;
+    }
+
+    public void heal(int nameProtect)
+    {
+        //have it store the index of who will protected for that round
+        //does some condition check
+    }
+}
