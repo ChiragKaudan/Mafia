13cec8b8f38a8c248fb7e45d465f5ffceead5fde
Tanya Gupta <tanya.gupta@gmail.com>
2021-11-07 14:54:06 -0800

    Implemented Singleton Design Pattern and made sure it worked.

--- Sheriff.java
+++ Sheriff.java
@@ -1,14 +1,22 @@
-package mafia;
+package Mafia;
 
 public class Sheriff extends Role
 {
-    public Sheriff()
+    private Sheriff()
     {
         this.isTown=true;
     }
 
+    private static Sheriff instance = new Sheriff();
+
+    public static Sheriff getInstance()
+    {
+        return instance;
+    }
+
     public boolean Interrogate()
     {
+        System.out.println("Test Sheriff object.");
         //insert code to sheriff to interrogate others
         return false;
     }
